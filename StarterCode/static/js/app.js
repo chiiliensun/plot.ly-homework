5900d4a9e81a5898409a8d433248fe91859c22dd
Chii-Lien Sun <chiilien.sun@gmail.com>
2021-04-16 13:30:15 -0700

    finished with getting horizontal plot working and samples loading correctly, fixed drop down change to work too

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,8 +1,8 @@
 
-// Use the D3 library to read in samples.json
-d3.json('samples2.json').then(data => {
-    console.log(data);
-})
+// Use the D3 library to read in samples.json - starter
+// d3.json('samples2.json').then(data => {
+//     console.log(data);
+// })
 
 // Initializes the page with a default plot
 function init() {
@@ -11,21 +11,47 @@ function init() {
   let dropdownMenu = d3.select("#selDataset");
 
   // Use D3 library to read in samples.json to select the dropdown menu
-  d3.json('samples.json').then(data => {
+  d3.json("samples2.json").then(data => {
 
       // Assign the value of the dropdown menu option to a variable
-      data.names.forEach(function(selectnames){
-        dropdownMenu.append("option").text(selectnames).property("value");
+      data.names.forEach(function(name) {
+        dropdownMenu.append("option").text(name).property("value");
       });
 
       // Set name from the list for functions to build plots
-      getPlot(data.names[0]);
-      getMeta(data.names[0]);
+      updatePlot(data.names[0]);
+      // updateMetaData(data.names[0]);
   });
 };
 
 // Function to read in data from samples.JSON for bar chart
-function barchartData
+function updatePlot(samples) {
+
+  // Use D3 library to read in samples.json
+    d3.json('samples2.json').then(data => {
+      // filtering data by the input value
+      let dataSamples = data.samples.filter(nameID => nameID.id === samples)[0];
+      let sampleValues = dataSamples.sample_values;
+      let otuIds = dataSamples.otu_ids;
+      let otuLabels = dataSamples.otu_labels;
+
+      // Trace1 to plot bar graph
+      let trace1 = [{
+        x: sampleValues.slice(0,10).reverse(),
+        y: otuIds.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+        type: "bar",
+        orientation: "h",
+        text: otuLabels
+        }];
+
+
+      let layout1 = {
+        title: "Top OTUs Found in an Individual"
+      };
+
+      Plotly.newPlot("bar", trace1, layout1);
+    });
+};
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
 
 // Use sample_values as the values for the bar chart
@@ -62,3 +88,8 @@ function barchartData
 
 
 // 6. Update all of the plots any time that a new sample is selected.
+function optionChanged(samples) {
+  updatePlot(samples);
+};
+
+init();
