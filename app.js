726f38eea431cbf18b331937917a8a8c8d2834a0
Chii-Lien Sun <chiilien.sun@gmail.com>
2021-04-17 10:30:12 -0700

    found in app file where reading json wasn't correct pathway

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function init() {
   let dropdownMenu = d3.select("#selDataset");
 
   // Use D3 library to read in samples.json to select the dropdown menu
-  d3.json("/samples2.json").then(data => {
+  d3.json("samples2.json").then(data => {
 
       // Assign the value of the dropdown menu option to a variable
       data.names.forEach(function(name) {
